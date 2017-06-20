44866575eafcd81c444c9aa107fc6fa394aa941e
count0ru <count0ru@gmail.com>
2017-06-20 17:33:00 +0300

    Update install_redis_tools.sh

--- install_redis_tools.sh
+++ install_redis_tools.sh
@@ -8,7 +8,7 @@ gem install redis
 alias redis-cluster-state='for number in " " -{1..6}; do echo -n "redis$number "; redis-cli -h redis$number cluster info | grep cluster_state; done'
 
 #remove user input from redis_trib
- sed -i 's/yes_or_die "Fix these slots by covering with a random node?"/#yes_or_die "Fix these slots by covering with a random node?"/' redis-trib.rb 
+sed -i 's/yes_or_die "Fix these slots by covering with a random node?"/#yes_or_die "Fix these slots by covering with a random node?"/' redis-trib.rb 
 
 for number in " " -{1..6}; do redis-cli -h redis$number cluster reset soft; done
 $(echo "./redis-trib.rb create --replicas 0 $(echo $(for number in " " -{1..6}; do host redis$number | awk '{print $4}'; done | sed ':a;N;$!ba;s/\n/:6379 /g' ):6379)")      
