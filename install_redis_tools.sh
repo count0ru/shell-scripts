a51c47a76d8b3e5d89e306abd5f29cfaedc2c7d5
count0ru <count0ru@gmail.com>
2017-06-20 16:12:15 +0300

    Update install_redis_tools.sh

--- install_redis_tools.sh
+++ install_redis_tools.sh
@@ -1,5 +1,9 @@
+MASK=$1
 apt-get update
-apt-get install ruby vim wget redis-tools -y
+apt-get install ruby vim wget redis-tools host -y
 wget http://download.redis.io/redis-stable/src/redis-trib.rb
 chmod +x redis-trib.rb
 gem install redis
+
+for number in " " -{1..6}; do redis-cli -h redis$number cluster reset soft; done
+$(echo "./redis-trib.rb create --replicas 0 $(echo $(for number in " " -{1..6}; do host redis$number | awk '{print $4}'; done | sed ':a;N;$!ba;s/\n/:6379 /g' ):6379)")      
