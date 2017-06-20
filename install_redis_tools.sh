f9dee804d120455957576209c17ab3315898f956
count0ru <count0ru@gmail.com>
2017-06-20 18:52:54 +0300

    Update install_redis_tools.sh

--- install_redis_tools.sh
+++ install_redis_tools.sh
@@ -1,16 +1,16 @@
 apt-get update
 apt-get install ruby vim wget redis-tools host -y
+cd $HOME
 wget http://download.redis.io/redis-stable/src/redis-trib.rb
 chmod +x redis-trib.rb
 gem install redis
 
-#show current cluster state 
+#aliases and functions for redis cluster
 alias redis-cluster-state='for number in " " -{1..6}; do echo -n "redis$number "; redis-cli -h redis$number cluster info | grep cluster_state; done'
-
-cd $HOME
+alias redis-cluster-reset='for number in " " -{1..6}; do echo -n "redis$number "; redis-cli -h redis$number cluster reset soft; done'
+function redis-cluster-cmd() { for number in " " -{1..6}; do echo -n "redis$number "; redis-cli -h redis$number $@; done }  
 
 #remove user input from redis_trib
 sed -i 's/yes_or_die "Fix these slots by covering with a random node?"/#yes_or_die "Fix these slots by covering with a random node?"/' redis-trib.rb 
 
-for number in " " -{1..6}; do redis-cli -h redis$number cluster reset soft; done
 $(echo "./redis-trib.rb create --replicas 0 $(echo $(for number in " " -{1..6}; do host redis$number | awk '{print $4}'; done | sed ':a;N;$!ba;s/\n/:6379 /g' ):6379)")      
