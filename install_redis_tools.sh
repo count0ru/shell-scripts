ea0ec8dbee6abc9a07e9a7ceecb4ed3c63d0ae63
count0ru <count0ru@gmail.com>
2017-06-20 15:52:56 +0300

    Update install_redis_tools.sh

--- install_redis_tools.sh
+++ install_redis_tools.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get install ruby vim wget redis-tools
+apt-get install ruby vim wget redis-tools -y
 wget http://download.redis.io/redis-stable/src/redis-trib.rb
 chmod +x redis-trib.rb
 gem install redis
